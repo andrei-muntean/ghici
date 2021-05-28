078aaa74634d05419ea7c2aa54c02c6f250fc9b3
Andrei Muntean <andrei_p_muntean@yahoo.com>
2021-05-28 20:37:51 +0300

    fix image name binding

--- src/app/riddle-view/result-dialog/result-dialog.component.ts
+++ src/app/riddle-view/result-dialog/result-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
 import { IDialogData } from '../../models';
 
 @Component({
@@ -11,7 +12,7 @@ export class ResultDialogComponent {
   constructor(
     private _dialogRef: MatDialogRef<ResultDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: IDialogData
-  ) {}
+  ) { }
 
   close(): void {
     this._dialogRef.close();
@@ -27,14 +28,18 @@ export class ResultDialogComponent {
     popupWin.document.open();
     popupWin.document.write(
       '\n <html>\n <head>\n <title>' +
-        this.data.correctAnswer +
-        '</title>\n  <style> img {max-width:550px;} </style>\n </head>\n ' +
-        '<body>' +
-        printContents +
-        '</body>\n      </html>'
+      this.data.correctAnswer +
+      '</title>\n  <style> img {max-width:550px;} </style>\n </head>\n ' +
+      '<body>' +
+      printContents +
+      '</body>\n      </html>'
     );
     setTimeout(() => {
       popupWin.print();
     }, 1000);
+  }
+
+  public get imageName(): string {
+    return this.data.correctImage;
   }
 }
